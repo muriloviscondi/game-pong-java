37fa2db209ca1126ca1c6dd8447c649e1b630453
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2020-04-08 15:50:20 -0300

    ball

---
+++ src/pong/Ball.java
@@ -0,0 +1,86 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
+
+public class Ball {
+	public double x, y;
+	public int width, height;
+	
+	public double dx, dy;
+	public double speed = 1;
+	
+	public Ball(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 5;
+		this.height = 5;
+		
+		int angle = new Random().nextInt(120 - 45) + 45;	
+		this.dx = Math.cos(Math.toRadians(angle));
+		this.dy = Math.sin(Math.toRadians(angle));
+	}
+	
+	public void tick() {
+		
+		if (x + (dx * speed) + width >= Game.WIDTH) {
+			dx *= -1;
+		}
+		else if (x + (dx * speed) < 0) {
+			dx *= -1;
+		}
+		
+		if (y >= Game.HEIGHT) {
+			// Ponto do Inimigo
+			System.out.println("Ponto do Inimigo");
+			new Game();
+			return;
+		}
+		else if (y < 0) {
+			// Ponto do Jogador
+			System.out.println("Ponto do Jogador");
+			new Game();
+			return;
+		}
+			
+		Rectangle bounds = new Rectangle ((int) (x + (dx * speed)), (int) (y + (dy * speed)), width, height);
+		
+		Rectangle boundsPlayer = new Rectangle(Game.player.x, Game.player.y, Game.player.width, Game.player.height);		
+		Rectangle boundsEnemy = new Rectangle((int) Game.enemy.x, (int) Game.enemy.y, Game.enemy.width, Game.enemy.height);
+		
+		if (bounds.intersects(boundsPlayer)) {
+			int angle = new Random().nextInt(120 - 45) + 45;	
+			this.dx = Math.cos(Math.toRadians(angle));
+			this.dy = Math.sin(Math.toRadians(angle));
+			
+			if (dy > 0) {
+				dy *= -1;
+				speed += 0.1;
+			}
+		}
+		else if (bounds.intersects(boundsEnemy)) {
+			int angle = new Random().nextInt(120 - 45) + 45;	
+			this.dx = Math.cos(Math.toRadians(angle));
+			this.dy = Math.sin(Math.toRadians(angle));
+			
+			if (dy < 0) {
+				dy *= -1;
+				speed += 0.1;
+			}
+		}
+		
+		x += dx * speed;
+		y += dy * speed;
+		
+		
+		
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillOval((int) x,  (int) y, width, height);
+	}
+
+}
